7d5f070f31aab94c757c888bf0b31f0312dc7257
Nikki Kerns <nkernns@admorgan.com>
2019-11-12 21:39:23 -0500

    Finished with Weather API Zip

--- public/main.js
+++ public/main.js
@@ -1,7 +1,37 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+const qs = x => document.querySelector(x)
+
+let url = 'https://api.openweathermap.org/data/2.5/weather?'
+let myApiKey = 'APPID=5a673c11a54e728d26522f385446c06a'
+let temp
+
+const randomColor = () => {
+  let x = Math.floor(Math.random() * 256)
+  let y = Math.floor(Math.random() * 256)
+  let z = Math.floor(Math.random() * 256)
+  let bgColor = 'rgb(' + x + ',' + y + ',' + z + ')'
+  console.log(bgColor)
+
+  // qs('.colorCode').textContent = bgColor
+  document.body.style.background = bgColor
+}
+randomColor()
+
+const getWeatherZip = async () => {
+  const zipCode = qs('input.zipInput').value
+
+  const response = await fetch(
+    `${url}zip=${zipCode}&${myApiKey}&units=imperial`
+  )
+  const weatherData = await response.json()
+  displayData(weatherData)
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const displayData = weatherData => {
+  qs('.temp').textContent = 'Temperature: ' + weatherData.main.temp + '°'
+  qs('.conditions').textContent =
+    'Conditions: ' + weatherData.weather[0].description
+  qs('.city').textContent = 'City: ' + weatherData.name
+}
+
+qs('.getWeatherZip').addEventListener('click', getWeatherZip)
+qs('.getWeatherZip').addEventListener('click', randomColor)
